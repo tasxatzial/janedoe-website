559f5177fcd9a062f2992866e9723b9577daeb69
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-03-25 21:32:40 +0200

    create hamburger icon

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ const navLinks = document.querySelectorAll('.nav-link');
 function toggleNav() {
     nav.classList.toggle('js-nav-open');
     document.body.classList.toggle('js-stop-scrolling');
+    navBtn.blur();
 }
 
 navBtn.addEventListener('click', toggleNav);
