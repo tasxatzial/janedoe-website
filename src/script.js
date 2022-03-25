baa2229b8f068a051c143f8f27fe0750f5ebbd06
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-03-25 06:55:21 +0200

    add open,close functionality to nav menu

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const navBtn = document.querySelector('.nav-button');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('.nav-link');
+
+function toggleNav() {
+    nav.classList.toggle('js-nav-open');
+    document.body.classList.toggle('js-stop-scrolling');
+}
+
+navBtn.addEventListener('click', toggleNav);
+
+navLinks.forEach(function(link) {
+    link.addEventListener('click', toggleNav);
+});
