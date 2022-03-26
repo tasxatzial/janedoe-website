da5518d039fe5605768feb95ba6bedc67c6b2d87
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-03-26 09:08:51 +0200

    add transitions to hamburger icon

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
 const navBtn = document.querySelector('.nav-button');
-const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('.nav-link');
 
 function toggleNav() {
-    nav.classList.toggle('js-nav-open');
+    document.body.classList.toggle('js-nav-open');
     document.body.classList.toggle('js-stop-scrolling');
     navBtn.blur();
 }
