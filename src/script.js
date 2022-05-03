62c74686c207957f7e299c21e4e1f2aacf38e0de
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-05-03 14:54:11 +0300

    use css to toggle visibility of nav links

--- src/script.js
+++ src/script.js
@@ -5,16 +5,10 @@ function toggleNav() {
     document.body.classList.toggle('js-nav-open');
     
     if (document.body.classList.contains('js-nav-open')) {
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].removeAttribute('hidden');
-        }
         navBtn.setAttribute('aria-label', 'close navigation');
         navBtn.setAttribute('aria-expanded', 'true');
         
     } else {
-        for (let i = 0; i < navLinks.length; i++) {
-            navLinks[i].setAttribute('hidden', '');
-        }
         navBtn.setAttribute('aria-label', 'open navigation');
         navBtn.setAttribute('aria-expanded', 'false');  
     }
