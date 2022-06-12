701640edf5551d1f2bddcc26d552e67fd1f70863
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-06-12 03:23:17 +0300

    trap focus inside nav when it's open

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,40 @@
+const nav = document.querySelector('nav');
 const navBtn = document.querySelector('.nav-button');
-const navLinks = document.querySelectorAll('.nav-link');
+const navLinks = nav.querySelectorAll('.nav-link');
+
+nav.addEventListener('keydown', function(e) {
+    if (e.key === 'Tab' || e.keyCode === 9) {
+        if ((e.shiftKey && document.activeElement === navLinks[0]) ||
+            (!e.shiftKey && document.activeElement === navLinks[navLinks.length - 1])) {
+                navBtn.focus();
+                e.preventDefault();
+            }
+        }
+    }
+);
+
+navBtn.addEventListener('keydown', function(e) {
+    if (document.body.classList.contains('js-nav-open')) {
+        if (e.key === 'Tab' || e.keyCode === 9) {
+            if (e.shiftKey) {
+                navLinks[navLinks.length - 1].focus();
+                e.preventDefault();
+            } else {
+                navLinks[0].focus();
+                e.preventDefault();
+            }
+        }
+    }
+});
 
 function toggleNav() {
     document.body.classList.toggle('js-nav-open');
-    
     if (document.body.classList.contains('js-nav-open')) {
+        document.body.focus();
         navBtn.setAttribute('aria-label', 'close navigation');
-        navBtn.setAttribute('aria-expanded', 'true');
-        
+        navBtn.setAttribute('aria-expanded', 'true'); 
     } else {
+        navBtn.focus();
         navBtn.setAttribute('aria-label', 'open navigation');
         navBtn.setAttribute('aria-expanded', 'false');  
     }
